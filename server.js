725582ffce5ddafa5944dc1c2e5edcf8f62b8500
Faizan Alam <faizansaikh786786f@gmail.com>
2025-10-26 22:03:53 +0530

    New Project1

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) => {
   });
 connectDb()
   // Start the server
-  const PORT =  4300;
+  const PORT = process.env.PORT || 4600
   app.listen(PORT, () => {
     console.log(`🚀 Server running on port ${PORT}`);
   });
